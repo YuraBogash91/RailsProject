2537ff9f2177b1cc2dd62b80ea9b115ee0021ba7
YuraBogash91 <yuriybogash93@gmail.com>
2013-07-15 20:23:09 +0300

    pass 17 screencast

--- my_store/app/controllers/items_controller.rb
+++ my_store/app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
+  before_filter :find_item, only:[:show, :edit, :update, :destroy, :upvote]
+  before_filter :check_if_admin, only:[:new, :edit, :update, :destroy, :create]
   def index
-  #  @items = Item.all
-  #  render text: @items.map{|i| "#{i.price}: #{i.name}" }.join("<br/>")
-  @items = Item.all
+     @items = Item.all
   end
   def new
     @item = Item.new
   end
   #items Post
+  def expensive
+    @items = Item.where("price>100")
+    render "index"
+  end
   def create
     @item = Item.create(params[:item])
     if @item.errors.empty?
@@ -21,18 +25,17 @@ class ItemsController < ApplicationController
 
   #items/id Get
   def show
-      unless @item = Item.where(id: params[:id]).first
+      unless @item
            render  text:  "Page not found" , status: 404
       end
   end
 
   #items/id Get
   def edit
-     @item = Item.find(params[:id])
+
   end
   #items/id PUT
   def update
-    @item = Item.find(params[:id])
     @item.update_attributes(params[:item])
     if @item.errors.empty?
       redirect_to item_path(@item)
@@ -42,7 +45,20 @@ class ItemsController < ApplicationController
   end
   #items/id DELETE
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
+    redirect_to "/items"
   end
+  def upvote
+      @item.increment!(:votes_count)
+      redirect_to action:  :index
+  end
+  private
+
+  def find_item
+    @item = Item.where(id: params[:id]).first
+    render_404 unless @item
+  end
+
+
+
 end
--- my_store/config/routes.rb
+++ my_store/config/routes.rb
@@ -1,5 +1,8 @@
 MyStore::Application.routes.draw do
 
-  resources :items
+  resources :items  do
+    get :upvote,    on: :member
+    get :expensive, on: :collection
+  end
 
 end
